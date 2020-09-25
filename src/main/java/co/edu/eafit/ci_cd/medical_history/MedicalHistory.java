849ccb75f3d21040565a5c2f882f41d9b5be3c91
Miguel Bolivar <mbo@localhost.localdomain>
2020-09-25 12:03:23 -0500

    Se agrega el plugin de Lombok

--- src/main/java/co/edu/eafit/ci_cd/medical_history/MedicalHistory.java
+++ src/main/java/co/edu/eafit/ci_cd/medical_history/MedicalHistory.java
@@ -1,27 +1,33 @@
 package co.edu.eafit.ci_cd.medical_history;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Builder;
+import lombok.Data;
 
-import java.io.Serializable;
+@Data
+@Builder
+public class MedicalHistory {
 
-public class MedicalHistory implements Serializable {
+    private final String id;
+    private final String clientName;
+    private final String clientId;
+    private final String symptoms;
 
-    @JsonProperty("id")
-    private String id;
-
-    @JsonProperty("clientName")
-    private String clientName;
-
-    @JsonProperty("clientId")
-    private String clientId;
-
-    @JsonProperty("symptoms")
-    private String symptoms;
-
-    public MedicalHistory(String id, String clientName, String clientId, String symptoms) {
-        this.id = id;
-        this.clientName = clientName;
-        this.clientId = clientId;
-        this.symptoms = symptoms;
-    }
+//    @JsonProperty("id")
+//    private String id;
+//
+//    @JsonProperty("clientName")
+//    private String clientName;
+//
+//    @JsonProperty("clientId")
+//    private String clientId;
+//
+//    @JsonProperty("symptoms")
+//    private String symptoms;
+//
+//    public MedicalHistory(String id, String clientName, String clientId, String symptoms) {
+//        this.id = id;
+//        this.clientName = clientName;
+//        this.clientId = clientId;
+//        this.symptoms = symptoms;
+//    }
 }
